8e23dabf09fcb941b6cc86e9e259b5829dafed5c
Emanuele <emanuele.dajello@gmail.com>
2022-06-06 22:24:20 +0200

    new

--- hello_x86/hello2.asm
+++ hello_x86/hello2.asm
@@ -1,14 +1,16 @@
 section .data
-    num1: equ 10
-    num2: equ 140
+    num1: equ 30
+    num2: equ 120
     msg:    db "Sum is correct", 10
 section .text      
-
     global _start
-
+    
 _start:
+    ;; rax = num1 
     mov rax, num1
+    ;; rax = num2
     mov rbx, num2
+    ;;rax = rax + rbx
     add rax, rbx
     cmp rax, 150
     jne .exit
